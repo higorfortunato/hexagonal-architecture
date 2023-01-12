6e34d9191a8bc7a40f671183e710adf41e445fbd
Higor Fortunato <higor.fortunato@compasso.com.br>
2023-01-12 16:02:53 -0300

    Implemented Web server Product methods

--- README.md
+++ README.md
@@ -1,18 +1,45 @@
 # hexagonal-architecture
 
+This is a demo project with the goal of demonstrate the main attributes of hexagonal architecture.
+
 ## Docker
 
+```sh
 docker-compose up -d
 docker exec -it appproduct bash
 docker-compose down
+```
 
-### Sqlite from Docker bash
+## Sqlite from Docker bash
 
+```sh
 sqlite3 db.sqlite
+```
 
 ## CLI
 
+```sh
 cobra-cli init
 cobra-cli add cli
 go run main.go cli -a=create -n="Product CLI" -p25.0
 go run main.go cli -a=get --id=e0854b6d-c349-481e-a32f-4b5e18357253
+```
+
+## WEBSERVER
+
+```sh
+cobra-cli add http
+go run main.go http
+```
+
+## Testing with Postman
+
+POST localhost:9000/product
+{
+"name": "Testando com Postman no",
+"price": 0
+}
+
+GET localhost:9000/product/06c7378c-76ce-4b7c-b27a-ac986bc08336/enable
+
+GET localhost:9000/product/06c7378c-76ce-4b7c-b27a-ac986bc08336/disable
---
+++ adapters/web/server/server.go
@@ -0,0 +1,44 @@
+package server
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	"github.com/higorfortunato/hexagonal-architecture/adapters/web/handler"
+	"github.com/higorfortunato/hexagonal-architecture/application"
+)
+
+type Webserver struct {
+	Service application.ProductServiceInterface
+}
+
+func MakeNewWebServer() *Webserver {
+	return &Webserver{}
+}
+
+func (w Webserver) Serve() {
+
+	r := mux.NewRouter()
+	n := negroni.New(
+		negroni.NewLogger(),
+	)
+
+	handler.MakeProductHandler(r, n, w.Service)
+	http.Handle("/", r)
+
+	server := &http.Server{
+		ReadHeaderTimeout: 10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		Addr:              ":9000",
+		Handler:           http.DefaultServeMux,
+		ErrorLog:          log.New(os.Stderr, "log: ", log.Lshortfile),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
